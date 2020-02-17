18d2243049ad3a16b1efa544ab4e607dfa8e2ec1
geertinckTommy <61147907+geertinckTommy@users.noreply.github.com>
2020-02-17 13:34:58 +0100

    updated with my info

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # UX portfolio template 1920
 
-## Name:
+## Name: Geerinck Tommy
 
-## Group:
+## Group: 1AVDa
 
 ## Getting Started
 
--- data.js
+++ data.js
@@ -10,10 +10,10 @@ const data = {
      */
 
     "general": {
-        "firstName": "Andrei",      // fill in with your own first name
-        "lastName": "Tarkovsky",    // fill in with your own last name
-        "class": "1NMD",            // fill in with your class/group e.g. 1AVDa, 1CMOa, 1NMD, ...
-        "studentnumber": "8675309"  // fill in with your student number
+        "firstName": "Tommy",      // fill in with your own first name
+        "lastName": "Geerinck",    // fill in with your own last name
+        "class": "1AVDa",            // fill in with your class/group e.g. 1AVDa, 1CMOa, 1NMD, ...
+        "studentnumber": "106232"  // fill in with your student number
     },
 
     /**
